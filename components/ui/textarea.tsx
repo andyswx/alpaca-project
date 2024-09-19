8c0a6ddc56e3f2af6682e91f706b8d1908c15321
andyswx <andrew.szwec@gmail.com>
2024-09-19 12:05:34 +0200

    fixing lint issues

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -3,9 +3,10 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 // export interface TextareaProps
-//   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+// extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+// const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+const Textarea = React.forwardRef<HTMLTextAreaElement>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
